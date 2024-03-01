213fa45dbbfcd03b7675bdf0e550062e43aaf23a
Dzaky <117564615+MagicfistCode@users.noreply.github.com>
2024-03-01 19:52:22 +0800

    Add project files.

---
+++ RandomNameGenerator/NamesGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNameGenerator
+{
+    public class NamesGenerator
+    {
+        public static string[] firstNames = {"Finn", "Jake", "Marceline", "Bonnibel" };
+        public static string[] lastNames = { "Mertens", "Dog", "Abadeer", "Bubblegum" };
+        public static string GenerateName()
+        {
+            var rand = new Random();
+            string randomFirstName = firstNames[rand.Next(firstNames.Length)];
+            string randomLastName = lastNames[rand.Next(firstNames.Length)];
+
+
+            return $"{randomFirstName} {randomLastName}";
+        }
+    }
+}
---
+++ RandomNameGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RandomNameGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomName = NamesGenerator.GenerateName();
+            Console.WriteLine(randomName);
+            Console.ReadLine();
+        }
+    }
+}
